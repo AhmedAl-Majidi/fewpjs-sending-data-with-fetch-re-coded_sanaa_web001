d427f1a1099f76420cd6887ffd97d03b26b87e93
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-16 05:12:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-function submitData(name, email) {
-
-    return fetch("http://localhost:3000/users", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-                name,
-                email
-            })
-        })
-        .then(response => {
-            response.json()
-        })
-        .then( function(object)
-        {document.body.innerHTML = object[1000]})
-        .catch( function ( error ) {
-          document.body.innerHTML = error.message
-          } )
+function submitData( name, email ) {
+  
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+             name,
+             email
+             
+      } )
+    } )
+    .then( function (response) {
+      return response.json()
+    } )
+    .then( function (object) {
+      document.body.innerHTML = object['id']
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
 }
-submitData();

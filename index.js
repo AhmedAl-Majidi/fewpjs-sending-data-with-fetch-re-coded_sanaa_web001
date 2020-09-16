bfaa3fd65aae14aa83052e8a04103805a7e708ec
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-16 04:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(username, userEmail) {
+
+    fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: username,
+                email: userEmail
+            })
+        })
+        .then(response => {
+            response.json();
+        })
+        .then(data => {
+          let p = document.createElement("p");
+                      let node = document.createTextNode(data[0]);
+                      p.appendChild(node);
+                      body.appendChild(p);
+        })
+}
+submitData();
